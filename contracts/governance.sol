1e0bbaf578e7751d6c5bd7d047358ebf06f6b8c3
mzawadi <mwaranguallan345@gmail.com>
2022-09-10 21:42:36 +0300

    update governance contract(fill function logic)

--- contracts/governance.sol
+++ contracts/governance.sol
@@ -14,11 +14,14 @@ contract governance{
     address public dPOPAddress;
     uint256 public minVoteCount;
     uint256 public maxVoteCount;
-    uint256 public proposalCount;
-    address[] public memberAddresses;
+    /// @notice integer to track proposalCount
+    uint256 private proposalCount;
+    /// @notice mapping to track membership if addresses
+    mapping(address => bool) public memberAddresses;
+    /// @notice mapping to track vote receipts of members
     mapping(address => Receipt) receipts;
     /// @notice mapping to easily track proposals
-    mapping(address => Proposal) proposals;
+    mapping(uint256 => Proposal) proposal;
 
     struct Proposal {
         /// @notice id for identifying proposal
@@ -27,8 +30,6 @@ contract governance{
         address proposer;
         /// @notice name of the proposal
         bytes32 name;
-        /// @notice number of votes a proposal has
-        uint256 votes;
         /// @notice Current number of votes in favor of this proposal
         uint forVotes;
         /// @notice Current number of votes in opposition to this proposal
@@ -37,6 +38,8 @@ contract governance{
         uint256 date;
         /// @notice date when proposal voting should be completed
         uint256 deadline;
+        /// @notice cancellation status of proposal
+        bool active;
         /// @notice cancellation status of proposal
         bool canceled;
         /// @notice execution status of proposal
@@ -54,7 +57,7 @@ contract governance{
         uint256 votes;
     }
 
-    enum voteOutcome {yes, maybe, no }
+    enum voteOutcome { yes, maybe, no }
 
     event ProposalCreated(uint256 id, address proposer, string description, uint256 date, uint256 deadline);
 
@@ -66,6 +69,7 @@ contract governance{
 
     constructor(address tknAddress){
         dPOPAddress = tknAddress;
+        proposalCount = 0;
     }
 
     /// @notice Explain to an end user what this does
@@ -74,15 +78,79 @@ contract governance{
     // @return Documents the return variables of a contract’s function state variable
     // @inheritdoc	Copies all missing tags from the base function (must be followed by the contract name)
 
-    function propose() public returns (bool) {}
-    function execute() public returns (bool) {}
-    function cancel() public returns (bool) {}
-    function getReceipts(uint256 proposalId, address voter) public returns (bool) {}
-    function getProposal(uint256 proposalId, address voter) public returns (bool) {}
-    function getProposalResult(uint256 proposalId, address voter) public returns (bool) {}
-    function state() public returns (bool) {}
-    function voteProposal() public payable returns (bool) {}
-    function delegateTokens() public returns (bool) {}
+    function addMember(address _member) public returns (bool) {
+        memberAddresses[_member] =  true;
+        return true;
+    }
 
+    function checkMember(address _account) public view returns (bool){
+        return memberAddresses[_account];
+    }
+
+    function propose(address _proposer, bytes32 _name, uint256 _date, uint256 _deadline) public returns (bool) {
+        require(memberAddresses[_proposer] =  true, "NonMember cannot propose!!");
+        uint256 currrentpProposalId = proposalCount;
+
+        // proposal[proposalCount++] = Proposal(
+        //     proposalCount++,
+        //     _proposer,
+        //     _name,
+        //     _date,
+        //     _deadline,
+        //     false,
+        //     false,
+        //     0,
+        //     receipts[_proposer] = 0
+        // );
+
+
+        proposalCount++;
+    }
+    function execute(uint256 _id) public returns (bool) {
+        require(_id> proposalCount, "Invalid ID!!");
+        // require(proposal[_id].canceled == false, "Proposal is not active");
+        Proposal storage currentProposal = proposal[_id];
+
+        currentProposal.executed = true;
+
+        return true;
+    }
+    function cancel(uint256 _id) public returns (bool) {
+        require(_id> proposalCount, "Invalid ID!!");
+
+        Proposal storage currentProposal = proposal[_id];
+
+        currentProposal.canceled = true;
+
+    }
+    function getReceipts(uint256 _proposalId, address _member) public view returns (bool, bool, uint256) {
+
+        Proposal storage currentProposal = proposal[_proposalId];
+
+        Receipt storage memberReceipt = receipts[_member];
+
+        return (memberReceipt.hasVoted, memberReceipt.support, memberReceipt.votes);
+
+    }
+    function getProposal(uint256 _proposalId, address _member) public view returns (bool) {
+        Proposal storage currentProposal = proposal[_proposalId];
+
+        return true;
+    }
+    function getProposalResult(uint256 proposalId, address voter) public returns (bool) {
+        Proposal storage currentProposal = proposal[proposalId];
+
+        require(currentProposal.forVotes > currentProposal.againstVotes, "The ney's have it");
+
+        return true;
+    }
+    function state(uint256 _id) public view returns (bool) {
+        Proposal storage currentProposal = proposal[_id];
+
+        return currentProposal.active;
+    }
+    function voteProposal(uint256 _id, address _member, uint256 _forVote, uint256 _againstVote) public payable returns (bool) {}
+    
+    // function delegateTokens() public returns (bool) {}
     
 }
