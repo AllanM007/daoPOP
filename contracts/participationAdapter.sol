8ce55a6891efe4e48040aebb959ca50a8d5a487d
mzawadi <mwaranguallan345@gmail.com>
2022-09-11 22:50:53 +0300

    update participationAdapter contract(70% done)

--- contracts/participationAdapter.sol
+++ contracts/participationAdapter.sol
@@ -18,8 +18,8 @@ contract participationAdpater{
     /// @notice value to store governance contract address
     address private governanceContract;
 
-    /// @notice mapping to track each member's contribution value
-    mapping(address => uint256) memberContribVal;
+    /// @notice mapping to track each member's participation value
+    mapping(address => uint256) memberParticipationVal;
 
     constructor(address _govContract){
         governanceContract = _govContract;
@@ -37,25 +37,42 @@ contract participationAdpater{
     /// @return true if the assignment works
     // @inheritdoc	Copies all missing tags from the base function (must be followed by the contract name)
 
+    /// @notice this formula sets the contribution value metric based on parameters set by the members from a (1-10) range
     function setproposalVal(uint256 _proposalValue) public onlyOwner returns (bool) {
+        require(_proposalValue > 10, "Invalid value above maximum");
+
         proposalVal = _proposalValue;
         
         return true;
     }
 
+    /// @notice this formula sets the vote value metric based on parameters set by the members from a (1-10) range
     function setvoteVal(uint256 _voteValue) public onlyOwner returns (bool) {
+        require(_voteValue > 10, "Invalid value above maximum");
+
         voteVal = _voteValue;
         
         return true;
     }
 
+    /// @notice this formula sets the contribution value metric based on parameters set by the members from a (1-10) range
     function setcontribVal(uint256 _contribValue) public onlyOwner returns (bool) {
+        require(_contribValue > 10, "Invalid value above maximum");
         contributionVal = _contribValue;
 
         return true;
     }
 
-    function calculateUserParticipation() public onlyOwner returns (bool) {
-     //m = v+p+c   
+    /// @notice this formula calculates the user participation metric based on parameters set by the members for 
+    /// each engegement criteria
+    function calculateUserParticipation(address _member) public onlyOwner returns (bool) {
+        //m = v+p+c
+        uint256 participationVal =  voteVal + proposalVal + contributionVal;
+        
+        /// @notice divide the cumulative metrics sum out of 30 to get a singular integer measurable metric for the D.A.O's social engagement
+        
+        memberParticipationVal[_member] = participationVal / 30;
+
+        return true;
     }
 }
